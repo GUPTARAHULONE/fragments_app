e81aeed4db4e7b0b1278ed297413f471b5ec94f6
GUPTARAHULONE <guptarahul26318@gmail.com>
2020-10-23 12:59:08 +0530

    fragments_use

---
+++ app/src/main/java/com/example/frament_1/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.frament_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements listfrag.ItemSelected {
+
+     TextView tvDescription;
+    String [] descripitions;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDescription=findViewById(R.id.tvDescription);
+
+        descripitions=getResources().getStringArray(R.array.descriptions);
+
+    if(findViewById(R.id.layout_portrait)!= null)
+        {
+            FragmentManager manager=this.getSupportFragmentManager();
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.detailFrag))
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .commit();
+        }
+    if(findViewById(R.id.layout_land)!=null)
+    {   FragmentManager manager=this.getSupportFragmentManager();
+        manager.beginTransaction()
+                .show(manager.findFragmentById(R.id.detailFrag))
+                .show(manager.findFragmentById(R.id.listFrag))
+                .commit();
+    }
+
+    }
+
+    @Override
+    public void onItemSelected(int index) {
+        tvDescription.setText(descripitions[index]);
+        if(findViewById(R.id.layout_portrait)!= null)
+        {
+            FragmentManager manager=this.getSupportFragmentManager();
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.detailFrag))
+                    .hide(manager.findFragmentById(R.id.listFrag))
+                    .addToBackStack(null)
+                    .commit();
+        }
+
+    }
+}
+
+
+
+
+
+
